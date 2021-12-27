60551f8d67e338299b3f486e9c00e6cb2f594dd7
Seth Mcfeeters <seth.mcfeeters@gmail.com>
2021-12-26 19:50:36 -0800

    errors now use anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
 mod cli;
-use structopt::StructOpt;
 mod tasks;
+
+use anyhow::anyhow;
+use structopt::StructOpt;
 use std::path::PathBuf;
 use cli::{Action::*, CommandLineArgs};
 use tasks::Task;
 
-fn find_default_journal_file() -> Options<Pathbuff> {
+
+fn find_default_journal_file() -> Option<PathBuf> {
     home::home_dir().map(|mut path| {
         path.push(".rusty-todo.json");
         path
     })
 }
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     // Get the command-line arguments.
     let CommandLineArgs {
         action,
@@ -22,13 +25,13 @@ fn main() {
     // Unpack the journal file.
     let journal_file = journal_file
     .or_else(find_default_journal_file)
-    .expect("Can not file todo file.");
+    .ok_or(anyhow!("Can not file todo file."))?;
 
     // Perform the action.
     match action {
         Add { text } => tasks::add_task(journal_file, Task::new(text)),
         List => tasks::list_tasks(journal_file),
         Done { position } => tasks::complete_task(journal_file, position),
-    }
-    .expect("Failed to perform action")
+    }?;
+    Ok(())
 }
